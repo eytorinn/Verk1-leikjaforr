0c5c0d0773bb023e3a494fd416d1c400195333a8
eytorinn <eytoralex@gmail.com>
2020-10-08 23:08:05 +0000

    Fyrsta (og síðasta) commit

---
+++ Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // hér breytum við nafni á Rigibody component í einfalt "rb"
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sideForce = 500f;
+    // "Start" gerist þegar við setjum leikinn í gang (Aðeins einu sinni í hverri keyrslu)
+    void Start()
+    {
+     
+    }
+    
+    // Breytti þessu í "FixedUpdate" því erum að leika okkur með þyngdarafl og hreyfingu
+    // "Update" eða "FixedUpdate" heldur áfram á meðan leikurinn er í gangi
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime); //sendum hlutinn áfram
+
+        if (Input.GetKey("d") )
+        {
+            rb.AddForce(sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
